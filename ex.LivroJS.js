51ee99cfd17aacf4cfb99e9792963557122002f3
Maria Raquel <94325823+mariaraquell@users.noreply.github.com>
2022-06-05 22:23:30 -0300

    Add files via upload

---
+++ ex.LivroJS.js
@@ -0,0 +1,11 @@
+<><meta charset="utf-8>" />
+
+<script>
+    // lê o valor do jantar
+    var jantar= Number(prompt("Valor do jantar R$: "));
+    // calcula os dados
+    var garcom= jantar * 0.10;
+    var total= jantar + garcom
+    //apresentar respostas
+    alert("Taxa de Garçom R$:" + garcom.toFixed(2) + "/nTotal R$: " + total.toFixed(2));
+</script></>
